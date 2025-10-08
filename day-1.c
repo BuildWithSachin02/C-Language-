600e63b87b367d2ce44a4a755876d9b2999325b5
sachin yadav <yadavsachin3166@gmail.com>
2025-10-08 16:16:18 +0530

    Add files via upload

---
+++ day-1.c
@@ -0,0 +1,103 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	printf("--------------------------------------- \n");
+	printf("Name: Sachin Yadav \n");
+	printf("Age: 22 \n");
+	printf("Courses: Full Stack Developer \n");
+	printf("Qualifiction: B.com \n");
+	printf("Add: 77, bhawani society godadara \n");
+	printf("Mob: 9054387845 \n");
+	printf("--------------------------------------- \n");
+	printf("\n");
+	printf("\n");
+	printf("\n");
+	printf("------------------- \n");
+	printf("|                 | \n");
+	printf("|                 | \n");
+	printf("|     SACHIN      | \n");
+	printf("|                 | \n");
+	printf("|                 | \n");
+	printf("------------------- \n");
+	printf("\n");
+	printf("\n");
+	printf("\n");
+	printf("------- \n");
+	printf("|      |\n");
+	printf("S      |\n");
+	printf("A      |\n");
+	printf("C      |\n");
+	printf("H      |\n");
+	printf("I      |\n");
+	printf("N      |\n");
+	printf("|      |\n");
+	printf("--------\n");
+	printf("\n");
+	printf("\n");
+	printf("\n");
+	printf("* \n");
+	printf("* * \n");
+	printf("* * * \n");
+	printf("* * \n");
+	printf("* \n");
+	printf("\n");
+	printf("\n");
+	printf("\n");
+	printf("* \n");
+	printf("* \n");
+	printf("*                                          * * \n");
+	printf("*                                        *     * \n");
+	printf("*           *  *                        * \n");
+	printf("*         *      *                   * \n");
+	printf("*       *          *               * \n");
+	printf("*    *                *         * \n");
+	printf("* *                      *   * \n");
+	printf("*                           \n");
+	printf("\n");
+	printf("\n");
+	printf("\n");
+	printf("* * * * * \n");
+	printf("*       * \n");
+	printf("* * * * * \n");
+	printf("*       * \n");
+	printf("*       * \n");
+	printf("*       * \n");
+	printf("*       * \n");
+	printf("\n");
+	printf("\n");
+	printf("\n");
+	printf("* * * \n");
+	printf("*     * \n");
+	printf("*   * \n");
+	printf("* * * \n");
+	printf("*    * \n");
+	printf("*     * \n");
+	printf("* * * \n");
+}
+	
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+
+
+
+
+
+
+
+
+
+
+
