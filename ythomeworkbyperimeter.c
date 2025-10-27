2e70ec3a85053538282f1a3a9197ef033165f74e
sachin yadav <yadavsachin3166@gmail.com>
2025-10-27 17:27:02 +0530

    Add files via upload

---
+++ ythomeworkbyperimeter.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<conio.h>
+
+/*
+Q.1
+ write a program to calculatec perimeter of rectangle take sides
+ a&b, from the user.
+*/
+
+void main(){
+	float a,b,perimeter;
+	/*taking input from user*/
+	printf("enter the lenth of rectangle(a):");
+	scanf("%f",&a);
+	printf("enter the lenth of rectangle(b):");
+	scanf("%f",&b);
+	/*formula*/
+	perimeter = 2 * (a + b);
+	printf("perimeter of rectangle=%.2f\n",perimeter);
+	
+	
+	
+	
+	
+	
+	
+	
+}
