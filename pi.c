0ccc47990d97bda56a2245bab4c5d33196ff4bd4
sachin yadav <yadavsachin3166@gmail.com>
2025-10-27 17:27:31 +0530

    Add files via upload

---
+++ pi.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+
+/*
+write a program to calculate area of circle.
+O-circle-radius-value of circle 3.14(22/7)
+Circle ke andar ka area = pr2(p is liye aa rha qki voh pi understan nhi kr rha h
+mene copy paste bhi kiya but boh uski value nhi le rha h p=3.14 hii h)
+
+*/
+
+void main(){
+	float radius;
+	printf("enter radius");
+	scanf("%f", &radius);
+	
+	printf("enter radius : %f",3.14*radius*radius);
+}
