2ef638d2dc4c5502b7c3e20a78589365462f107a
sachin yadav <yadavsachin3166@gmail.com>
2025-10-28 15:33:14 +0530

    Add files via upload

---
+++ ythwlargesttwonumbers.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<conio.h>
+
+/*
+yt-homework Q.1
+	Write a program to find the largest of two numbers.
+*/
+
+int main(){
+	int a,b;
+	/*user input*/
+	printf("enter the two numbers: ");
+	scanf("%d %d",&a,&b);
+	/*(isdigit) and if-else we are using*/
+	if(a>b)
+		printf("%d is the largest numbers.\n");
+	
+	else if(b<a)
+		printf("%d is the larget numbers.\n");
+	else
+		printf("%d is the largest numbers.\n");	
+	
+	
+	
+	
+	
+		
+}
