8a5e99f3c260fec7c22ad1f813c844f855f1de7a
sachin yadav <yadavsachin3166@gmail.com>
2025-10-29 17:07:13 +0530

    Add files via upload

---
+++ evennumber.c.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+
+/*
+	abb hme even number ko print krwana h 1 to 100 tak iske baad odd ;
+*/
+int main (){
+	int i;
+	printf("enter the even number: ");
+	scanf("%d",&i);
+	
+	for(i = 1; i <= 100; i++){
+		if(i % 2 == 0){
+			printf("%d\n",i);
+		}
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+}
