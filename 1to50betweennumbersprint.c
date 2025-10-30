e1a9dfed74debd84a00b3ad2104653f427b6f684
sachin yadav <yadavsachin3166@gmail.com>
2025-10-30 17:15:39 +0530

    Add files via upload

---
+++ 1to50betweennumbersprint.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+
+/*
+	Write a program to print even numbers between 1 to 50.
+*/
+
+int main(){
+	int i;
+	for(i = 1; i <= 50; i++){
+		printf("%d \n",i);
+	}
+}
