f93d76f1826dbc99d1806bc0965bc31e78ccbd38
sachin yadav <yadavsachin3166@gmail.com>
2025-11-02 13:56:32 +0530

    Add files via upload

---
+++ practicemcqerrosfinds.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main(){
+	int x = 2;
+	
+	if(x == 1){
+		printf("x is equal to 1");
+	}
+	else{
+		printf("x is not equal to 1");
+	}
+}
+/*
+	 mtlb yeh kyy krha h ki jab x=2 h
+	 thik h fir hmne print krwaya ki x is equal to 1 
+	 yeh print hone ke baad memory me x ki value =1 ho jati h qki hmne 
+	 (= single equal rkha h )
+*/
