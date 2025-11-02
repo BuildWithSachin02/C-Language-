29edb967ad339b96d94c412a8f1266a9b210a5b0
sachin yadav <yadavsachin3166@gmail.com>
2025-11-02 13:53:54 +0530

    Add files via upload

---
+++ forloop.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/*
+	three types of loops:-
+		1.for loop, 2.while loop, 3.do while loop
+		so we're learning on for loop:-
+*/
+int main(){
+	int i;
+	for(i = 1; i <= 5; i = i+1){
+		printf("hello sachin.\n");
+	}
+}
