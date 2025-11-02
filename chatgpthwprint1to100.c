51b274185f7199099e0f51572765e306a3f43112
sachin yadav <yadavsachin3166@gmail.com>
2025-11-02 14:02:26 +0530

    Add files via upload

---
+++ chatgpthwprint1to100.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+
+/*
+	chatgpt-questions:-
+	Write a program to print numbers from 1 to 100.
+*/
+int main (){
+	int i;/*i yeh hmra counter h */
+	
+	printf("if you want print a number to 1 to 100. : ");
+	scanf("%d",&i);
+	
+	for(i = 1; i <=100; i++){
+		printf("%d\n",i);
+	}
+	
+	
+	
+}
